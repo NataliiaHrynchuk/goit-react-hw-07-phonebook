8ef2fff7c64d5a57c9e335700231ef99537ddddc
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-22 13:51:41 +0200

    Create Filter.js

---
+++ src/components/Filter.js
@@ -0,0 +1,34 @@
+import {Text} from 'components/Text.styled';
+import {Label} from 'components/Label.styled';
+import { Box } from 'components/Box.styled';
+import { getFilter } from 'redux/filter/filterSelectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/filter/filterSlice';
+
+export const Filter = () => {
+    const dispatch = useDispatch();
+    const filter = useSelector(getFilter);
+    const changeFilter = event => {
+        dispatch(setFilter(event.currentTarget.value));
+    };
+    return (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+            w="300px"
+            gap="10px"
+        >
+            <Label htmlFor="id-filter"
+            >
+                Find contacts by name
+            </Label>
+            <Text
+                type="text"
+                name="filter"
+                id="id-filter"
+                value={filter}
+                onChange={changeFilter}
+            ></Text>
+        </Box>
+    )
+};
